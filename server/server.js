b9abff297232c85cd7bb88d1a6e0e1907e98baf7
dannyhz92 <d.hernandez@lafilm.edu>
2018-08-29 19:52:22 -0700

    added a PUT route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 var express = require("express");
 var bodyParser = require("body-parser");
 const {ObjectID} = require("mongodb")
@@ -67,10 +68,33 @@ app.delete("/todos/:id", (req, res)=>{
 
      res.status(400).send();
   })
-  // remove todo by id
-  // success
-  // error;;
-  //  400 with empty body
+
+});
+
+app.patch("/todos/:id",(req, res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+      return res.status(404).send();
+
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+      body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set:body},{new: true}).then((todo)=>{
+      if(!todo){
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    }).catch((e)=>{
+      res.status(400).send();
+    })
 });
 
 app.listen(port, ()=> {
